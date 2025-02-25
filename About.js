cf79c36ac16b18f2551c1a39e2c6ca2207cacbec
Joemunga <133594633+sunjoe508@users.noreply.github.com>
2025-02-26 00:52:56 +0300

    Create About.js

---
+++ About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Learn more about our farm management system and how we help farmers manage their products efficiently.</p>
+    </div>
+  );
+};
+
+export default About;
