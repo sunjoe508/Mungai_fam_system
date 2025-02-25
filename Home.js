c94c11ae7c9000c201ddbca8b2bf1c3d13e6fdf3
Joemunga <133594633+sunjoe508@users.noreply.github.com>
2025-02-26 00:52:24 +0300

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Farm Management System</h1>
+      <p>Buy and manage farm products easily.</p>
+    </div>
+  );
+};
+
+export default Home;
